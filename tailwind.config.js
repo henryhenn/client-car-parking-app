0259065671e2509cc9709edd96a4f35dca2e080b
= <=>
2023-05-10 18:57:39 +0700

    Installed & Configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: { colors },
+  },
+  plugins: [],
+}
